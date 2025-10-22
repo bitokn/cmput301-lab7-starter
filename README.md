89564616a30261efedbc99f938bf2d0f52e1d385
Bernabe Kondrak <b.kondrak45@gmail.com>
2025-10-22 13:44:39 -0600

    commit changes made in the lab and add readme stuff

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Android-UI-testing
 Android UI Testing - CMPUT 301 Lab 7   
 Compile SDK - 34
+
+# CMPUT 301 : Lab 7 - Android UI Testing with Espresso
+
+## Student Details
+
+- **Full Name:** `Bernabe Kondrak`
+- **CCID:** `bkondrak`
+
+## References and Resources
+
+List any resources used here, or simply put `N/A` if not applicable.
+
+## Verbal Collaboration
+
+| Student Name | CCID     |
+| ------------ | -------- |
+| `<example1>` | `<CCID>` |
+| `<example2>` | `<CCID>` |
--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -1,7 +1,87 @@
 package com.example.androiduitesting;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
-    
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void testAddCity() {
+        // click the add button
+        onView(withId(R.id.button_add)).perform(click());
+        // type edmonton
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+
+        //press confirm
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        //check if edmonton is in the list
+        onView(withText("Edmonton")).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void testClearCity(){
+        // add a city
+        onView(withId(R.id.button_add)).perform(click());
+
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+
+        onView(withId(R.id.button_confirm)).perform(click());
+
+
+        // add another
+        onView(withId(R.id.button_add)).perform(click());
+
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Calgary"));
+
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        // click clear city button
+
+        onView(withId(R.id.button_clear)).perform(click());
+
+        // check if it doesnt exist
+        onView(withText("Edmonton")).check(doesNotExist());
+        onView(withText("Calgary")).check(doesNotExist());
+
+
+
+
+    }
+
+    @Test
+    public void testListView(){
+        // Add a city
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.button_confirm)).perform(click());
+        // Check if in the Adapter view (given id of that adapter view) there is a data
+        // (which is an instance of String) located at position zero.
+        // If this data matches the text we provided then Voila! Our test should pass
+        // You can also use anything() in place of is(instanceOf(String.class))
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list)).atPosition(0).check(matches((withText("Edmonton"))));
+    }
 }
